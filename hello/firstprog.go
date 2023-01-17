5446fa8e328d9fb94f34113fe0182a64710aa5cc
Alekhya Gandra <alekhyareddy@Alekhyas-MacBook-Air.local>
2023-01-17 16:38:33 -0600

    made changes

--- hello/firstprog.go
+++ hello/firstprog.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+func sum(x int, y int) int {
+	z := x + y
+	return z
+}
 func main() {
 	fmt.Println("hello")
+
+	c := sum
+	a := c(1, 2)
+	fmt.Println(a)
+
+	//	time.Sleep( time.Minute)
+
+	func(x int) {
+		fmt.Println(x)
+	}(1)
+
+	func() {
+		fmt.Println("hi")
+	}()
+
+	//fmt.Println()
+
 }
